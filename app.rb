d285216a0142776337e2b4824dd16b00a3ea731a
Aliff <1211201920@student.mmu.edu.my>
2023-09-14 10:03:39 +0800

    Games can be played in rounds

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+enable :sessions  
 
 get '/' do 
     erb :index
@@ -7,33 +8,49 @@ end
 post '/getstarted' do
     @name = params[:name]
     # if @user_name == 
+    @rounds = params[:rounds].to_i
+    
+    session[:name] = @name
+    session[:rounds] = @rounds
+    session[:results] = []
     erb :game
 end
 
 post '/result' do
-
+    
     @player_choice = params[:choice]
-    @computer_choice = ["rock", "paper", "scissors"].sample
+    @computer_choice = ["Rock", "Paper", "Scissors"].sample
 
+    results = check(@player_choice, @computer_choice)
+    history = {Player: @player_choice, Computer: @computer_choice, results: @outcome}
+    session[:results] << history
+
+    if session[:results].length < session[:rounds]
+        erb :game
+    else
+        record = session[:results].map {|hash| hash[:results]}
+        session[:win_count] = record.count("You won!")
+        session[:lose_count] = record.count("You lose!")
+        session[:draw_count] = record.count("Draw!")
+
+        erb :result
+    end
+  
+end
+
+def check(player, computer)
     case
-    when (@player_choice == 'rock' && @computer_choice == 'scissors') ||
-        (@player_choice == 'scissors' && @computer_choice == 'paper') ||
-        (@player_choice == 'paper' && @computer_choice == 'rock')
+    when (player == 'Rock' && computer == 'Scissors') ||
+        (player == 'Scissors' && computer == 'paper') ||
+        (player == 'Paper' && computer == 'Rock')
     @outcome = "You won!"
-    when (@player_choice == 'scissors' && @computer_choice == 'rock') ||
-        (@player_choice == 'paper' && @computer_choice == 'scissors') ||
-        (@player_choice == 'rock' && @computer_choice == 'paper')
+    when (player == 'Scissors' && computer == 'Rock') ||
+        (player == 'Paper' && computer == 'Scissors') ||
+        (player == 'Rock' && computer == 'Paper')
     @outcome = "You lose!"
     else
     @outcome = "Draw!"
+    
     end
-
     puts @outcome
-  
-  erb :result
-end
-
-
-
-
-
+    end
